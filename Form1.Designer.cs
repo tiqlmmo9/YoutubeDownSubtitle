c57e7a057ba9cbce002b92605ac6740aa427933b
Phuoc <phuocich.dev@gmail.com>
2024-09-27 15:10:37 +0700

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace YouTubeSubtitleDownloaderWinForm
+{
+    partial class Form1 : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button btnDownload;
+        private Button btnCancel;
+        private TextBox logTextBox;
+        private TextBox playlistIdTextBox;
+        private TextBox outputPathTextBox;
+        private Label playlistIdLabel;
+        private Label outputPathLabel;
+        private RadioButton radioPlaylist;
+        private RadioButton radioVideo;
+        private TextBox videoIdTextBox;
+        private Label videoIdLabel;
+        private Button btnBrowse;
+        private FolderBrowserDialog folderBrowserDialog;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            btnDownload = new Button();
+            btnCancel = new Button();
+            logTextBox = new TextBox();
+            playlistIdTextBox = new TextBox();
+            outputPathTextBox = new TextBox();
+            playlistIdLabel = new Label();
+            outputPathLabel = new Label();
+            radioPlaylist = new RadioButton();
+            radioVideo = new RadioButton();
+            videoIdTextBox = new TextBox();
+            videoIdLabel = new Label();
+            btnBrowse = new Button();
+            folderBrowserDialog = new FolderBrowserDialog();
+            SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            btnDownload.Location = new System.Drawing.Point(12, 193);
+            btnDownload.Name = "btnDownload";
+            btnDownload.Size = new System.Drawing.Size(416, 23);
+            btnDownload.TabIndex = 0;
+            btnDownload.Text = "Download Subtitles";
+            btnDownload.UseVisualStyleBackColor = true;
+            btnDownload.Click += btnDownload_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Anchor = ((AnchorStyles)((AnchorStyles.Top | AnchorStyles.Right)));
+            btnCancel.Location = new System.Drawing.Point(462, 193);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new System.Drawing.Size(117, 23);
+            btnCancel.TabIndex = 9;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // logTextBox
+            // 
+            logTextBox.Anchor = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right)));
+            logTextBox.Location = new System.Drawing.Point(12, 238);
+            logTextBox.Multiline = true;
+            logTextBox.Name = "logTextBox";
+            logTextBox.ScrollBars = ScrollBars.Vertical;
+            logTextBox.Size = new System.Drawing.Size(567, 194);
+            logTextBox.TabIndex = 1;
+            // 
+            // playlistIdTextBox
+            // 
+            playlistIdTextBox.Anchor = ((AnchorStyles)(((AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right)));
+            playlistIdTextBox.Location = new System.Drawing.Point(12, 57);
+            playlistIdTextBox.Multiline = true;
+            playlistIdTextBox.Name = "playlistIdTextBox";
+            playlistIdTextBox.ScrollBars = ScrollBars.Vertical;
+            playlistIdTextBox.Size = new System.Drawing.Size(567, 75);
+            playlistIdTextBox.TabIndex = 2;
+            // 
+            // outputPathTextBox
+            // 
+            outputPathTextBox.Anchor = ((AnchorStyles)(((AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right)));
+            outputPathTextBox.Location = new System.Drawing.Point(12, 153);
+            outputPathTextBox.Name = "outputPathTextBox";
+            outputPathTextBox.Size = new System.Drawing.Size(416, 23);
+            outputPathTextBox.TabIndex = 3;
+            // 
+            // playlistIdLabel
+            // 
+            playlistIdLabel.AutoSize = true;
+            playlistIdLabel.Location = new System.Drawing.Point(12, 39);
+            playlistIdLabel.Name = "playlistIdLabel";
+            playlistIdLabel.Size = new System.Drawing.Size(84, 15);
+            playlistIdLabel.TabIndex = 4;
+            playlistIdLabel.Text = "Playlist URL(s):";
+            // 
+            // outputPathLabel
+            // 
+            outputPathLabel.AutoSize = true;
+            outputPathLabel.Location = new System.Drawing.Point(12, 135);
+            outputPathLabel.Name = "outputPathLabel";
+            outputPathLabel.Size = new System.Drawing.Size(75, 15);
+            outputPathLabel.TabIndex = 5;
+            outputPathLabel.Text = "Output Path:";
+            // 
+            // radioPlaylist
+            // 
+            radioPlaylist.AutoSize = true;
+            radioPlaylist.Checked = true;
+            radioPlaylist.Location = new System.Drawing.Point(12, 12);
+            radioPlaylist.Name = "radioPlaylist";
+            radioPlaylist.Size = new System.Drawing.Size(62, 19);
+            radioPlaylist.TabIndex = 6;
+            radioPlaylist.TabStop = true;
+            radioPlaylist.Text = "Playlist";
+            radioPlaylist.UseVisualStyleBackColor = true;
+            radioPlaylist.CheckedChanged += radioPlaylist_CheckedChanged;
+            // 
+            // radioVideo
+            // 
+            radioVideo.AutoSize = true;
+            radioVideo.Location = new System.Drawing.Point(81, 12);
+            radioVideo.Name = "radioVideo";
+            radioVideo.Size = new System.Drawing.Size(55, 19);
+            radioVideo.TabIndex = 7;
+            radioVideo.TabStop = true;
+            radioVideo.Text = "Video";
+            radioVideo.UseVisualStyleBackColor = true;
+            radioVideo.CheckedChanged += radioVideo_CheckedChanged;
+            // 
+            // videoIdTextBox
+            // 
+            videoIdTextBox.Anchor = ((AnchorStyles)(((AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right)));
+            videoIdTextBox.Location = new System.Drawing.Point(12, 57);
+            videoIdTextBox.Multiline = true;
+            videoIdTextBox.Name = "videoIdTextBox";
+            videoIdTextBox.ScrollBars = ScrollBars.Vertical;
+            videoIdTextBox.Size = new System.Drawing.Size(567, 75);
+            videoIdTextBox.TabIndex = 8;
+            // 
+            // videoIdLabel
+            // 
+            videoIdLabel.AutoSize = true;
+            videoIdLabel.Location = new System.Drawing.Point(12, 39);
+            videoIdLabel.Name = "videoIdLabel";
+            videoIdLabel.Size = new System.Drawing.Size(77, 15);
+            videoIdLabel.TabIndex = 4;
+            videoIdLabel.Text = "Video URL(s):";
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Anchor = ((AnchorStyles)((AnchorStyles.Top | AnchorStyles.Right)));
+            btnBrowse.Location = new System.Drawing.Point(462, 153);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new System.Drawing.Size(117, 23);
+            btnBrowse.TabIndex = 10;
+            btnBrowse.Text = "Browse...";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(595, 461);
+            Controls.Add(radioVideo);
+            Controls.Add(radioPlaylist);
+            Controls.Add(outputPathLabel);
+            Controls.Add(playlistIdLabel);
+            Controls.Add(outputPathTextBox);
+            Controls.Add(playlistIdTextBox);
+            Controls.Add(videoIdTextBox);
+            Controls.Add(videoIdLabel);
+            Controls.Add(logTextBox);
+            Controls.Add(btnDownload);
+            Controls.Add(btnCancel);
+            Controls.Add(btnBrowse);
+            FormBorderStyle = FormBorderStyle.FixedSingle; // Set form border style to FixedSingle
+            MaximizeBox = false; // Disable maximize box
+            Name = "Form1";
+            Text = "YouTube Subtitle Downloader";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+        private void radioPlaylist_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioPlaylist.Checked)
+            {
+                // Show playlist ID textbox and hide video ID textbox
+                playlistIdLabel.Visible = true;
+                playlistIdTextBox.Visible = true;
+                videoIdTextBox.Visible = false;
+                videoIdLabel.Visible = false;
+            }
+        }
+
+        private void radioVideo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioVideo.Checked)
+            {
+                // Show video ID textbox and hide playlist ID textbox
+                playlistIdLabel.Visible = false;
+                playlistIdTextBox.Visible = false;
+                videoIdTextBox.Visible = true;
+                videoIdLabel.Visible = true;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputPathTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        //private void btnDownload_Click(object sender, EventArgs e)
+        //{
+        //    // Download logic here
+        //}
+
+        //private void btnCancel_Click(object sender, EventArgs e)
+        //{
+        //    // Cancel logic here
+        //}
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace YouTubeSubtitleDownloaderWinForm
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource cancellationTokenSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Update your Form1.cs file
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+
+                string apiKey = "AIzaSyA4HGKpLTz76bL-xaXKCzbu9DGI1YtSJVA";  // Replace with your Google API key
+
+                string[] idStrs;
+                string outputDirectory = outputPathTextBox.Text.Trim(); // Get the output directory from the TextBox
+
+                if (radioPlaylist.Checked)
+                {
+                    idStrs = playlistIdTextBox.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                else if (radioVideo.Checked)
+                {
+                    idStrs = videoIdTextBox.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                else
+                {
+                    MessageBox.Show("Please select Playlist or Video.");
+                    return;
+                }
+
+                if (idStrs.Length == 0)
+                {
+                    MessageBox.Show("Please enter IDs.");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(outputDirectory))
+                {
+                    MessageBox.Show("Please enter an output directory.");
+                    return;
+                }
+
+                if (radioPlaylist.Checked)
+                    await ProcessForPlaylist(apiKey, idStrs, outputDirectory, cancellationTokenSource.Token);
+
+                else if (radioVideo.Checked)
+                    await ProcessForVideo(apiKey, idStrs, outputDirectory, cancellationTokenSource.Token);
+
+                logTextBox.AppendText("DOWNLOAD COMPLETE!!!\r\n");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error occurred while processing the request. Please check your input and try again.");
+                return;
+            }
+        }
+
+        private async Task ProcessForPlaylist(string apiKey, string[] idStrs, string outputDirectory, CancellationToken cancellationToken)
+        {
+            int folderIndex = 1;
+
+            foreach (string playlistStr in idStrs)
+            {
+                string playlistId = ExtractPlaylistIdOrVideoId(playlistStr);
+
+                if (string.IsNullOrEmpty(playlistId))
+                {
+                    MessageBox.Show("Please enter a playlist ID.");
+                    return;
+                }
+
+                var (videos, playlistTitle) = await GetVideosAndTitleFromPlaylist(apiKey, playlistId);
+
+                string playlistDirectory = Path.Combine(outputDirectory, $"{folderIndex:000}. {playlistTitle}");
+                Directory.CreateDirectory(playlistDirectory);
+
+                logTextBox.AppendText($"Videos in the playlist '{playlistTitle}':\r\n");
+
+                int videoIndex = 1;
+
+                // Collect tasks for extracting subtitles for each video
+                var extractSubTasks = new List<Task>();
+                foreach (var video in videos)
+                {
+                    var task = ExtractSubForVideo(playlistDirectory, video, videoIndex, cancellationToken);
+                    extractSubTasks.Add(task);
+                    videoIndex++;
+                }
+
+                // Wait for all tasks to complete
+                await Task.WhenAll(extractSubTasks);
+
+                folderIndex++;
+            }
+        }
+
+        private async Task ProcessForVideo(string apiKey, string[] idStrs, string outputDirectory, CancellationToken cancellationToken)
+        {
+            var downloadTasks = idStrs.Select((videoStr, index) => DownloadAndExtractSubForVideo(apiKey, outputDirectory, videoStr, index + 1, cancellationToken));
+            await Task.WhenAll(downloadTasks);
+        }
+
+        private async Task DownloadAndExtractSubForVideo(string apiKey, string outputDirectory, string videoStr, int videoIndex, CancellationToken cancellationToken)
+        {
+            string videoId = ExtractPlaylistIdOrVideoId(videoStr);
+
+            if (string.IsNullOrEmpty(videoId))
+            {
+                MessageBox.Show("Invalid video ID.");
+                return;
+            }
+
+            var (video, videoTitle) = await GetVideoAndTitle(apiKey, videoId, cancellationToken);
+
+            await ExtractSubForVideo(outputDirectory, video, videoIndex, cancellationToken);
+        }
+
+        private async Task ExtractSubForVideo(string directory, VideoInfo video, int videoIndex, CancellationToken cancellationToken)
+        {
+            logTextBox.AppendText($"{videoIndex}. {video.Title} ({video.Id})\r\n");
+
+            string url = $"https://youtubetranscript.com/?server_vid2={video.Id}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url, cancellationToken);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    XDocument xmlDoc = XDocument.Parse(responseBody);
+
+                    var transcriptText = new System.Text.StringBuilder();
+                    foreach (var element in xmlDoc.Descendants("text"))
+                    {
+                        transcriptText.AppendLine(element.Value);
+                    }
+
+                    string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+                    string videoDirectory = Path.Combine(directory, $"{videoIndex:000}. {sanitizedTitle}.txt");
+
+                    await File.WriteAllTextAsync(videoDirectory, transcriptText.ToString(), cancellationToken);
+
+                    logTextBox.AppendText($"Saved transcript to {videoDirectory}\r\n");
+                }
+                catch (HttpRequestException ex)
+                {
+                    logTextBox.AppendText("Request error: " + ex.Message + "\r\n");
+                }
+                catch (Exception ex)
+                {
+                    logTextBox.AppendText("Parsing error: " + ex.Message + "\r\n");
+                }
+            }
+        }
+
+        public static async Task<(List<VideoInfo>, string)> GetVideosAndTitleFromPlaylist(string apiKey, string playlistId)
+        {
+            List<VideoInfo> videos = new List<VideoInfo>();
+            string playlistTitle = "";
+
+            using (var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = apiKey,
+                ApplicationName = "YouTubePlaylistVideoIds"
+            }))
+            {
+                var playlistRequest = youtubeService.Playlists.List("snippet");
+                playlistRequest.Id = playlistId;
+                var playlistResponse = await playlistRequest.ExecuteAsync();
+                if (playlistResponse.Items.Count > 0)
+                {
+                    playlistTitle = playlistResponse.Items[0].Snippet.Title;
+                }
+
+                string nextPageToken = "";
+                while (nextPageToken != null)
+                {
+                    var request = youtubeService.PlaylistItems.List("contentDetails,snippet");
+                    request.PlaylistId = playlistId;
+                    request.MaxResults = 50;
+                    request.PageToken = nextPageToken;
+
+                    var response = await request.ExecuteAsync();
+                    foreach (var item in response.Items)
+                    {
+                        videos.Add(new VideoInfo
+                        {
+                            Id = item.ContentDetails.VideoId,
+                            Title = item.Snippet.Title
+                        });
+                    }
+
+                    nextPageToken = response.NextPageToken;
+                }
+            }
+
+            return (videos, playlistTitle);
+        }
+
+        public static async Task<(VideoInfo, string)> GetVideoAndTitle(string apiKey, string videoId, CancellationToken cancellationToken)
+        {
+            VideoInfo video = null;
+            string videoTitle = "";
+
+            using (var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = apiKey,
+                ApplicationName = "YouTubeVideoInfo"
+            }))
+            {
+                var videoRequest = youtubeService.Videos.List("snippet");
+                videoRequest.Id = videoId;
+                var videoResponse = await videoRequest.ExecuteAsync(cancellationToken);
+                if (videoResponse.Items.Count > 0)
+                {
+                    videoTitle = videoResponse.Items[0].Snippet.Title;
+                    video = new VideoInfo
+                    {
+                        Id = videoId,
+                        Title = videoTitle
+                    };
+                }
+            }
+
+            return (video, videoTitle);
+        }
+
+        public static string RemoveInvalidPathChars(string title)
+        {
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            return string.Concat(title.Select(c => invalidChars.Contains(c) ? '_' : c));
+        }
+
+        static string ExtractPlaylistIdOrVideoId(string playlistUrl)
+        {
+            // Split the URL by '=' and take the last part
+            string[] parts = playlistUrl.Split('=');
+            if (parts.Length > 1)
+            {
+                return parts[parts.Length - 1];
+            }
+
+            // If '=' is not found, split by '/' and take the last part
+            parts = playlistUrl.Split('/');
+            return parts[parts.Length - 1];
+        }
+
+        private void playlistIdLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (cancellationTokenSource != null && !cancellationTokenSource.IsCancellationRequested)
+            {
+                cancellationTokenSource.Cancel();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class VideoInfo
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Index { get; set; }
+    }
+}
